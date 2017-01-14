d70443d7b0b3464e7d33143cdd622378249879d5
Daniel Zerlett <daniel@zerlett.eu>
2017-01-14 14:27:23 +0100

    deep sleep

---
+++ config_example.h
@@ -0,0 +1,6 @@
+#define AP_SSID "SSID-OF-WLAN"
+#define AP_PASSWORD "PASSWORD"
+#define DEEP_SLEEP 10000000
+#define SERVER_IP "192.168.1.100"
+#define SERVER_PORT 80
+#define THERMOMETER_ID "esp8266test1"
